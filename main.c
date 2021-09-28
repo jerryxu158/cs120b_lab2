fbd9a805c1c508a72c962a3873703d00a235d577
jerry <jxu158@ucr.edu>
2021-09-28 15:52:40 -0700

    first commit

---
+++ main.c
@@ -0,0 +1,23 @@
+/*	Author: Jerry Xu
+ *  Partner(s) Name: 
+ *	Lab Section: 22
+ *	Assignment: Lab 2 Exercise 
+ *	Exercise Description: [optional - include for your own benefit]
+ *
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
+#include <avr/io.h>
+#ifdef _SIMULATE_
+#include "simAVRHeader.h"
+#endif
+
+int main(void) {
+    /* Insert DDR and PORT initializations */
+
+    /* Insert your solution below */
+    while (1) {
+	
+    }
+    return 1;
+}
